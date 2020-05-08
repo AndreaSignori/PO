b12622f797789c29d201154c06fb825161609c73
SigManCrazy <crashctr92@gmail.com>
2020-05-08 16:29:04 +0200

    prod

--- prodotto.cpp
+++ prodotto.cpp
@@ -18,30 +18,6 @@ int Prodotto::GetDiscount() const {
 	return discount;
 }
 
-<<<<<<< HEAD
-void Prodotto::SetDiscount(int newdiscount) {
-    /*if (newdiscount || newdiscount < 0) {
-		discount = 0;
-		return;
-	}
-	if (newdiscount > 100) {
-		discount = 100;
-		return;
-	}
-	discount = newdiscount;
-    return;*/
-    switch (newdiscount) {
-    case ( < 0):
-        discount = 0;
-        break;
-    case (newdiscount > 100):
-        discount = 100;
-    break;
-    default:
-        discount = newdiscount;
-        break;
-    }
-=======
 void Prodotto::SetCodice(string newcodice) {
 
 }
@@ -54,5 +30,5 @@ void Prodotto::SetDiscount(int newdiscount = 0) {
 	default:
 		break;
 	}
->>>>>>> 48dbae5ac0a053ec1b52f48df843c5a0f8b8f7ed
+
 }
