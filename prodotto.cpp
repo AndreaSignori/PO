2404bb8b8ff15b6355cb09693fa8d6322ef0e8a1
Andrea Signori <signori.andrea@icloud.com>
2020-05-08 15:56:55 +0200

    Modifiche a prodotto.h e cpp

--- prodotto.cpp
+++ prodotto.cpp
@@ -1,8 +1,32 @@
 #include "prodotto.h"
-#include <iostream>
 
 using namespace std;
 
-Prodotto::Prodotto(Prodotto *ptr){
-    id = ptr->GetId();
+std::string Prodotto::GetCodice() const {
+	return codice;
+}
+
+std::string Prodotto::GetNome() const {
+	return nome;
+}
+
+float Prodotto::GetPrezzo() const {
+	return prezzo;
+}
+
+int Prodotto::GetDiscount() const {
+	return discount;
+}
+
+void Prodotto::SetDiscount(int newdiscount) {
+	if (newdiscount || newdiscount < 0) {
+		discount = 0;
+		return;
+	}
+	if (newdiscount > 100) {
+		discount = 100;
+		return;
+	}
+	discount = newdiscount;
+	return;
 }
--- prodotto.h
+++ prodotto.h
@@ -1,15 +1,27 @@
 #ifndef PRODOTTO_H
 #define PRODOTTO_H
 
+#include <string>
+
 class Prodotto
 {
 private:
-    int id;
+    std::string codice;
+    std::string nome;
+    float prezzo;
+    int discount;
+    std::string casaprod;
+    char foto;
+
 public:
-    Prodotto() {}
-    Prodotto(int x):id(int(x)) {}
-    Prodotto(Prodotto* ptr) ;
-    virtual int GetId() const{};
+    Prodotto();
+    Prodotto(int x);
     virtual ~Prodotto() = 0;
+    std::string GetCodice() const;
+    std::string GetNome() const;
+    float GetPrezzo() const;
+    int GetDiscount() const;
+    void SetDiscount(int newdiscount);
 };
 #endif // PRODOTTO_H
+
