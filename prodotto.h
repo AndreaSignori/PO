55871ce8df8ea10bf2885a2c9791c2e88bdc5168
Andrea Signori <signori.andrea@icloud.com>
2020-05-28 15:33:44 +0200

    Tutte le classi sono in prodotto.h

--- prodotto.h
+++ prodotto.h
@@ -36,3 +36,124 @@ public:
 };
 #endif // PRODOTTO_H
 
+#ifndef PRODCHIMICO_H
+#define PRODCHIMICO_H
+#include "prodotto.h"
+
+class ProdChimico : virtual public Prodotto
+{
+private:
+    int quantitaml;
+    bool tossico;
+
+public:
+    ProdChimico() {}
+    ~ProdChimico() {}
+
+    int GetQuantita() const;
+    bool GetTossico() const;
+
+    void SetTossico(bool toxic);
+    void SetQuantita(int quantita);
+};
+#endif // PRODCHIMICO_H
+
+#ifndef ATTREZZATURA_H
+#define ATTREZZATURA_H
+#include "prodotto.h"
+
+class Attrezzatura : virtual public Prodotto
+{
+private:
+
+public:
+    Attrezzatura() {}
+
+    ~Attrezzatura() {}
+};
+#endif // ATTREZZATURA_H
+
+#ifndef SHAMPOO_H
+#define SHAMPOO_H
+#include "prodchimico.h"
+
+class Shampoo : virtual public ProdChimico
+{
+private:
+    enum TipoCapelli { GENERALE, RICCI, LISCI, COLORATI, ROVITANTI };
+    static char const tipocapelli_nome[9][4];
+    enum TipoShampoo { VOLUMIZZANTE, ANTICADUTA, SEBOREGOLATORE, ANTIFORFORA, COLORANTE };
+    static char const tiposhampoo_nome[20][4];
+public:
+    Shampoo() {}
+    ~Shampoo() {}
+
+    /*Set e Get*/
+    TipoCapelli getTipoCapelli() const;
+    virtual TipoShampoo getTipoShampoo() const;
+
+    void setTipoCapelli(const TipoCapelli& type);
+    void setTipoShampoo(const TipoShampoo& type);
+};
+#endif // SHAMPOO_H
+
+#ifndef TINTE_H
+#define TINTE_H
+#include "prodchimico.h"
+
+class Tinte : virtual public ProdChimico
+{
+private:
+
+public:
+    Tinte() {}
+
+    ~Tinte() {}
+};
+#endif // TINTE_H
+
+#ifndef SHAMCOLOR_H
+#define SHAMCOLOR_H
+#include "tinte.h"
+#include "shampoo.h"
+
+class ShamColor : public Tinte, public Shampoo
+{
+private:
+
+public:
+    ShamColor() {}
+
+    ~ShamColor() {}
+};
+#endif // SHAMCOLOR_H
+
+#ifndef STRUMENTO_H
+#define STRUMENTO_H
+#include "attrezzatura.h"
+
+class Strumento : public Attrezzatura
+{
+private:
+
+public:
+    Strumento() {};
+
+    ~Strumento() {};
+};
+#endif // STRUMENTO_H
+
+#ifndef ACCESSORIO_H
+#define ACCESSORIO_H
+#include "attrezzatura.h"
+
+class Accessorio : public Attrezzatura
+{
+private:
+
+public:
+    Accessorio() {}
+
+    ~Accessorio() {}
+};
+#endif // ACCESSORIO_H
